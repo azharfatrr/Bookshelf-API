5ad90294868fa11d8000689d70b054567dd69363
azharfatrr <m.azhar.faturahman@gmail.com>
2021-05-29 19:59:07 +0700

    add queryHandler

--- src/handler/getAllBooksHandler.js
+++ src/handler/getAllBooksHandler.js
@@ -4,17 +4,35 @@ const books = require('../books');
 
 
 const getAllBooksHandler = (request, h) => {
-  const booksInfo = [];
+  let booksInfo = books;
+  const {name, reading, finished} = request.query;
 
-  books.forEach((book) => {
-    const {id, name, publisher} = book;
-    booksInfo.push({id, name, publisher});
-  });
+  // Terdapat query ?name
+  if (name !== undefined) {
+    booksInfo = booksInfo.filter(
+        (book) => book.name.toLowerCase().includes(name.toLowerCase()));
+  };
+
+  // Terdapat query ?reading
+  if (reading !== undefined) {
+    booksInfo = booksInfo.filter((book) => book.reading == reading);
+  }
+
+  // Terdapat query ?finished
+  if (finished !== undefined) {
+    booksInfo = booksInfo.filter((book) => book.finished == finished);
+  }
 
   const response = h.response({
     status: 'success',
     data: {
-      books: booksInfo,
+      books: booksInfo.map((book) => (
+        {
+          id: book.id,
+          name: book.name,
+          publisher: book.publisher,
+        }),
+      ),
     },
   });
   response.code(200);
